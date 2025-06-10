5a8de6a197f842525834f55838122d18a5ca876b
Giancarlo Catalano <g.a.catalano@stir.ac.uk>
2025-06-10 09:25:57 +0100

    Implemented data collection for genome threshold

---
+++ initial_testing/data_collection_25_06_9/data_collect_genome_search.py
@@ -0,0 +1,75 @@
+import json
+import os
+
+import utils
+from Gian_experimental.NSGAIICustom.testing_in_vitro.SPRef import OptimisedSPref
+from PolishSystem.read_data import get_pRef_from_vectors
+from initial_testing.data_collection_25_06_9.genome_threshold_search import PolishSearchSettings, \
+    search_for_pss_using_genome_threshold, results_to_json
+
+cluster_info_file_name = r"C:\Users\gac8\PycharmProjects\PSSearch\data\retail_forecasting\cluster_info_250_random.pkl"
+data_path = r"C:\Users\gac8\PycharmProjects\PSSearch\data\retail_forecasting"
+results_path = r"C:\Users\gac8\PycharmProjects\PSSearch\initial_testing\data_collection_25_06_9\results\v1"
+
+def run(destination_path):
+    print("Starting data collection")
+
+    all_new = PolishSearchSettings(code_name="all_new",
+                                   population_size=100,
+                                   evaluation_budget=500,
+                                   genome_threshold=3,
+                                   cluster_info_file_name=cluster_info_file_name,
+                                   use_custom_atomicity=True,
+                                   use_custom_sampling_operator=True,
+                                   use_custom_mutation_operator=True,
+                                   use_custom_crossover_operator=True,
+                                   include_ps_len=True,
+                                   include_sample_quantity=True,
+                                   include_wasserstein_distance=True,
+                                   include_diff_median=True,
+                                   include_atomicity=True)
+
+    all_old = PolishSearchSettings(code_name="all_old",
+                                   population_size=100,
+                                   evaluation_budget=500,
+                                   cluster_info_file_name=cluster_info_file_name,
+                                   include_sample_quantity=True,
+                                   include_variance=True,
+                                   include_atomicity=True)
+
+
+    configs = [all_old, all_new]
+
+    def in_path(path):
+        return os.path.join(data_path, path)
+    train_pRef = get_pRef_from_vectors(name_of_vectors_file=in_path("train_many_hot_vectors_250_random.csv"),
+                                       name_of_fitness_file=in_path("train_fitness_250_random.csv"),
+                                       column_in_fitness_file=2)
+    test_pRef = get_pRef_from_vectors(name_of_vectors_file=in_path("test_many_hot_vectors_250_random.csv"),
+                                      name_of_fitness_file=in_path("test_fitness_250_random.csv"),
+                                      column_in_fitness_file=2)
+
+    train_SPRef = OptimisedSPref.from_pRef(train_pRef)
+    test_SPRef = OptimisedSPref.from_pRef(test_pRef)
+
+    collected_data = []
+    for config in configs:
+        print(f"Running for settings {config.code_name}")
+        settings_json = config.to_dict()
+        result = search_for_pss_using_genome_threshold(train_session_data=train_pRef,
+                                                       optimised_session_data=train_SPRef,
+                                                       search_settings=config)
+        result_json = results_to_json(result, search_settings=config,
+                                      test_session_data=test_SPRef)
+        collected_data.append({"config": settings_json,
+                "results": result_json})
+
+
+    destination_file = os.path.join(destination_path, "run_"+utils.get_formatted_timestamp()+".json")
+    with utils.open_and_make_directories(destination_file) as file:
+        json.dump(collected_data, file, indent=4)
+
+    print("All done!")
+
+
+run(results_path)
--- initial_testing/data_collection_25_06_9/genome_threshold_search.py
+++ initial_testing/data_collection_25_06_9/genome_threshold_search.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from typing import Optional, Iterable
 
 import numpy as np
-from scipy.stats import wasserstein_distance
+from scipy.stats import wasserstein_distance, mannwhitneyu
 
 from Core.PRef import PRef
 from Gian_experimental.NSGAIICustom.CustomOperators import NCSamplerFromPRef, NCMutationCounterproductive, \
@@ -17,27 +17,30 @@ from PolishSystem.OperatorsBasedOnSimilarities.similarities_utils import gian_ge
 
 @dataclass
 class PolishSearchSettings:
+    code_name: str
     population_size: int
     evaluation_budget: int
 
-    use_custom_sampling_operator: bool
-    use_custom_mutation_operator: bool
-    use_custom_crossover_operator: bool
-
-    use_custom_atomicity: bool
-    genome_threshold: Optional[int]
-
-    include_ps_len: bool
-    include_sample_quantity: bool
-    include_wasserstein_distance: bool
-    include_diff_median: bool
-    include_atomicity: bool
-
-    include_p_value: bool
-    include_mean_fitness: bool
-    include_variance: bool
-
     cluster_info_file_name: str
+
+    use_custom_sampling_operator: bool = False
+    use_custom_mutation_operator: bool = False
+    use_custom_crossover_operator: bool = False
+
+    use_custom_atomicity: bool = False
+    genome_threshold: Optional[int] = None
+
+    include_ps_len: bool = False
+    include_sample_quantity: bool = False
+    include_wasserstein_distance: bool = False
+    include_diff_median: bool = False
+    include_atomicity: bool = False
+
+    include_p_value: bool = False
+    include_mean_fitness: bool = False
+    include_variance: bool = False
+
+
 
     def to_dict(self):
         return vars(self)
@@ -83,18 +86,18 @@ def make_metrics_cached(metrics):
     return get_values
 
 
-def search_for_pss_using_genome_threshold(session_data: PRef,
+def search_for_pss_using_genome_threshold(train_session_data: PRef,
                                           optimised_session_data: OptimisedSPref,
                                           search_settings: PolishSearchSettings) -> list[EvaluatedNCSolution]:
 
-    n = session_data.search_space.amount_of_parameters
+    n = train_session_data.search_space.amount_of_parameters
     def tie_breaker(population: Iterable[EvaluatedNCSolution], quantity_required: int):
         return heapq.nsmallest(iterable=population, key=lambda x: x.fitnesses[0], n=quantity_required)
 
     similarities = gian_get_similarities(search_settings.cluster_info_file_name)
     transition_matrix = get_transition_matrix(similarities)
 
-    custom_sampling = NCSamplerFromPRef.from_PRef(session_data)
+    custom_sampling = NCSamplerFromPRef.from_PRef(train_session_data)
     custom_mutation = NCMutationCounterproductive(transition_matrix)
     custom_crossover = NCCrossoverTransition(transition_matrix)
 
@@ -129,6 +132,10 @@ def search_for_pss_using_genome_threshold(session_data: PRef,
         if search_settings.include_mean_fitness:
             result.append(-np.average(matching))
 
+        if search_settings.include_p_value:
+            test = mannwhitneyu(matching, non_matching, alternative="greater")
+            result.append(test.pvalue)
+
         if search_settings.include_wasserstein_distance:
             distance = wasserstein_distance(matching, non_matching) if min(len(matching), len(non_matching)) > 2 else 0
             result.append(-distance)
@@ -145,14 +152,18 @@ def search_for_pss_using_genome_threshold(session_data: PRef,
             atomicity_score = atomicity(ps)
             result.append(-atomicity_score)
 
+
+
+
+
         return tuple(result)
 
     algorithm = NSGAIICustom(sampling=custom_sampling if search_settings.use_custom_sampling_operator else traditional_sampling,
                              mutation=custom_mutation if search_settings.use_custom_mutation_operator else traditional_mutation,
-                             crossover=custom_mutation if search_settings.use_custom_mutation_operator else traditional_crossover,
+                             crossover=custom_crossover if search_settings.use_custom_mutation_operator else traditional_crossover,
                              probability_of_crossover=0.5,
-                             eval_budget=5000,
-                             pop_size=100,
+                             eval_budget=search_settings.evaluation_budget,
+                             pop_size=search_settings.population_size,
                              tournament_size=3,
                              mo_fitness_function=make_metrics_cached(get_metrics),
                              unique=True,
@@ -162,3 +173,24 @@ def search_for_pss_using_genome_threshold(session_data: PRef,
 
     run_results = algorithm.run()
     return run_results
+
+
+
+def results_to_json(results_of_run: Iterable[EvaluatedNCSolution],
+                    test_session_data: OptimisedSPref,
+                    search_settings: PolishSearchSettings) -> list[dict]:
+    def ps_to_json(run_result: EvaluatedNCSolution) -> dict:
+        ps = run_result.solution
+        matching, non_matching = test_session_data.partition(ps, threshold=search_settings.genome_threshold)
+        can_do_stats = min(len(matching), len(non_matching)) > 2
+        if can_do_stats:
+            return {"ps": sorted(list(ps)),
+                        "samples": len(matching) / len(non_matching)}
+        else:
+            test = mannwhitneyu(matching, non_matching, alternative="greater")
+            return {"ps": ps,
+                    "median_diff": np.median(matching) - np.median(non_matching),
+                    "p_value": test.pvalue,
+                    "samples": len(matching) / len(non_matching)}
+
+    return list(map(ps_to_json, results_of_run))
